b3d8ea61f148e70d8cab570f9978e7042676fe13
Robert Bloy <robert.bloy@freenet.de>
2017-07-13 07:44:25 +0200

    deallocate statt stop

--- stopCluster.sh
+++ stopCluster.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-az vm stop --name infranode3 --resource-group ocpv3group
-az vm stop --name infranode2 --resource-group ocpv3group
-az vm stop --name infranode1 --resource-group ocpv3group
-az vm stop --name node03 --resource-group ocpv3group
-az vm stop --name node02 --resource-group ocpv3group
-az vm stop --name node01 --resource-group ocpv3group
-az vm stop --name master1 --resource-group ocpv3group
-az vm stop --name master2 --resource-group ocpv3group
-az vm stop --name master3 --resource-group ocpv3group
-az vm stop --name bastion --resource-group ocpv3group
+az vm deallocate --name infranode3 --resource-group ocpv3group
+az vm deallocate --name infranode2 --resource-group ocpv3group
+az vm deallocate --name infranode1 --resource-group ocpv3group
+az vm deallocate --name node03 --resource-group ocpv3group
+az vm deallocate --name node02 --resource-group ocpv3group
+az vm deallocate --name node01 --resource-group ocpv3group
+az vm deallocate --name master1 --resource-group ocpv3group
+az vm deallocate --name master2 --resource-group ocpv3group
+az vm deallocate --name master3 --resource-group ocpv3group
+az vm deallocate --name bastion --resource-group ocpv3group
